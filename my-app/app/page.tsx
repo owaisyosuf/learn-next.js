93ac0a71c314c39773665f27b01b34c4c5297787
owaisyosuf <owais.251990@gmail.com>
2024-09-24 18:28:20 +0500

    flex box

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -10,40 +10,47 @@ export default function Home() {
       <div id="form">
         {" "}
         {/*form start here*/}
-        <form action="">
-          <label>Name</label>
-          <input type="text" name="" id="" placeholder="enter your name" />{" "}
-          <br />
-          <label> Father Name</label>
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="enter your father name"
-          />
-          <br />
-          <label>Age </label>
-          <input type="number" name="" id="" min={18} max={60} />
-          <br />
-          <label>contact # </label>
-          <input type="number" name="" id="" />
-          <label>gender</label>
-          <br />
-          <input type="radio" name="gender" id="" /> male
-          <input type="radio" name="gender" id="" /> female
-          <label>email address</label>
-          <br />
-          <input type="email" name="" id="" />
-          <label>country</label>
-          <br />
-          <select name="" id="">
-            <option value="">karachi</option>
-            <option value="">hydrabad</option>
-            <option value="">lahore</option>
-          </select>
-          <br />
-          <button>submit</button>
-        </form>
+        <div id="form1">
+          <form action="">
+            <label>Name</label>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="enter your name"
+            />{" "}
+            <br />
+            <label> Father Name</label>
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="enter your father name"
+            />
+            <br />
+            <label>Age </label>
+            <input type="number" name="" id="" min={18} max={60} />
+            <br />
+            <label>contact # </label>
+            <input type="number" name="" id="" />
+            <br />
+            <label>gender</label>
+            <input type="radio" name="gender" id="" /> male
+            <input type="radio" name="gender" id="" /> female
+            <br />
+            <label>email address</label>
+            <input type="email" name="" id="" />
+            <br />
+            <label>country</label>
+            <select name="" id="">
+              <option value="">karachi</option>
+              <option value="">hydrabad</option>
+              <option value="">lahore</option>
+            </select>
+            <br />
+            <button>submit</button>
+          </form>
+        </div>
       </div>
     </div>
   );
